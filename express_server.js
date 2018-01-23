0dcce3d417532c6ab09ea35ccf32d43ba22fa232
Kenneth <krbrown@live.ca>
2018-01-23 22:53:06 +0000

    with random string generator

--- express_server.js
+++ express_server.js
@@ -1,20 +1,23 @@
 var express = require("express");
 var app = express();
 var PORT = process.env.PORT || 8080; // default port 8080
+var randomstring = require("randomstring");
+// var generateRandomString = randomstring.generate(6);
 
 app.set("view engine", "ejs");
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
+function generateRandomString(length) {
+ return(randomstring.generate(length));
+}
+
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
-// app.get("/", (req, res) => {
-//   res.end("Hello!");
-// });
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
@@ -30,7 +33,7 @@ app.get("/urls/:id", (req, res) => {
 });
 
 app.post("/urls", (req, res) => {
-	console.log(req.body); //debug statement to see POST parameters
+	console.log(req.body, generateRandomString(6)); //debug statement to see POST parameters
 	res.send("Ok");
 });
 
